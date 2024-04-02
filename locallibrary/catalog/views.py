e747a13b3562a0d58163b268aa3de2caacabe99e
Grant Bittner <bitzgrant@gmail.com>
2024-04-02 13:47:31 -0500

    Sessions framework

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -17,6 +17,10 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
